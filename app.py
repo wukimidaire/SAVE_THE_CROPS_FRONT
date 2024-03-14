4c38eef0ce018b0c1e9f16f7fff56ddf2dd926ff
Victor De Coster <victor.decoster94@gmail.com>
2024-03-14 11:33:58 +0100

    chatbotopenaiandlayoutchanges

--- app.py
+++ app.py
@@ -1,21 +1,32 @@
 import streamlit as st
+from streamlit_chat import message
 import os # needed for the file paths
 import pandas as pd
 import requests
 import time # needed for adding a delay (optional)
 import base64
 from requests_toolbelt.multipart.encoder import MultipartEncoder
-import json
 
-st.set_page_config(page_title = "Save The Crops", page_icon="✨", layout = "centered", initial_sidebar_state = "expanded")
+import openai
+
+
+
+st.set_page_config(
+    page_title = "Save The Crops",
+    page_icon="✨",
+    layout = "centered",
+    initial_sidebar_state = "expanded")
+
+
+# Secrets variables
+api_url = os.environ["API_URL"]
+webhook_url = st.secrets["WEBHOOK_URL"]
+openai.api_key = st.secrets["API_OPENAI"]
 
 
 # construct the relative path of the backgroun image
 base_dir = os.path.dirname(os.path.realpath(__file__))
 img_path = os.path.join(base_dir, "media", "field.jpg")
-# Secrets variables
-api_url = os.environ["API_URL"]
-webhook_url = st.secrets["WEBHOOK_URL"]
 
 
 def set_bg_image(main_bg):
@@ -43,17 +54,13 @@ def set_bg_image(main_bg):
      )
 set_bg_image(img_path)
 
-def send_message(message):
-    """Sends a message to the chatbot backend."""
-    response = requests.post(webhook_url, json={"message": message})
-    return response.json()
 
 def send_image_to_api(image_data, api_url=api_url):
     try:
         multipart_data = MultipartEncoder(
             fields={
                 "file": (image_data.name, image_data, "image/jpeg"),
-                "plant": options
+                "plant": options[0]
             }
         )
         headers = {"Content-Type": multipart_data.content_type}
@@ -66,14 +73,25 @@ def send_image_to_api(image_data, api_url=api_url):
         st.error(f"An error occurred while sending the image: {e}")
         return None
 
+def api_calling(prompt):
+    completions = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+    message = completions.choices[0].text
+    return message
 
 # This is providing a shaded background so there is more contrast on the buttons and text fields
 css_body_container = f'''
 <style>
     section [data-testid="stAppViewBlockContainer"] {{background-color:rgba(0, 66, 37, 0.6)}}
-
 </style>
 '''
+
 st.markdown(css_body_container,unsafe_allow_html=True)
 
 
@@ -81,16 +99,15 @@ st.markdown(css_body_container,unsafe_allow_html=True)
 container = st.container(height=1000)
 
 with container:
-    '''
-    # Save The Crops
-    '''
+
+    st.markdown("<h1 style='text-align: center; '>Save The Crops</h1>", unsafe_allow_html=True)
 
     options = st.multiselect(
-    'What plant are you uploading?',
-    ['tomato', 'maize', 'cassava', 'cashew', 'all'], max_selections=1)
+        "What plant are you uploading?",
+        ["tomato", "maize", "cassava", "cashew", "all"],
+        max_selections=1
+        )
     uploaded_image = st.file_uploader("Choose an Image", type=["jpg", "jpeg", "png"])
-
-
 
     if uploaded_image is not None:
         if not options:  # Check if a specie has been selected
@@ -98,24 +115,35 @@ with container:
         else:
             selected_specie = options[0]  # Get the selected specie
 
-        if True:
-        # try:
+        if True:  # replace with the actual condition to check response
             response = send_image_to_api(uploaded_image, api_url)
             if response:
-                st.write(f"Disease : {response['disease']}")
+                if response["disease"] == "healthy":
+                    message = "Grab a beer, your plants are fine!"
+
+                else:
+                    message = f"Disease : {response['disease']}"
+                    #st.subheader(f"{(response['disease'].capitalize())}")
+                st.subheader(message)
                 st.image(uploaded_image, width=400)
-        # except requests.exceptions.RequestException as e:
-        #     st.error(f"An error occurred: {e}")
 
+    st.title("ChatGPT ChatBot With Streamlit and OpenAI")
+    if 'user_input' not in st.session_state:
+        st.session_state['user_input'] = []
 
+    if 'openai_response' not in st.session_state:
+        st.session_state['openai_response'] = []
 
+    def get_text():
+        input_text = st.text_input("write here", key="input")
+        return input_text
 
-    # Wrap the user input and submit button within a form
-    #with st.form(key="chatbot_form"):  # Give the form a unique key
-    #    user_input = st.text_input("Ask a question to the chatbot:", "")
-    #    submit_button = st.form_submit_button(label="Send to Chatbot")
+    user_input = get_text()
 
-    #if user_input:
-    #    if submit_button:  # Check if the submit button was pressed
-     #       response = send_message(user_input)
-     #       st.write("Chatbot:", response["message"])
+    if user_input:
+        output = api_calling(user_input)
+        output = output.lstrip("\n")
+
+        # Store the output
+        st.session_state.openai_response.append(user_input)
+        st.session_state.user_input.append(output)
